1e1be6768d7af2a901bb6b995d15215d7b366dc4
Prempal Singh <prempal.42@gmail.com>
2015-10-30 06:20:52 +0530

    [fixme] receiver

--- app/src/main/java/osc/dtuwifi/MainActivity.java
+++ app/src/main/java/osc/dtuwifi/MainActivity.java
@@ -1,20 +1,15 @@
 package osc.dtuwifi;
 
-import android.net.http.SslError;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.webkit.SslErrorHandler;
-import android.webkit.WebChromeClient;
-import android.webkit.WebResourceError;
-import android.webkit.WebResourceRequest;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,44 +20,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final WebView webView = (WebView) findViewById(R.id.webView);
         final EditText username = (EditText) findViewById(R.id.username);
         final EditText password = (EditText) findViewById(R.id.password);
         Button submit = (Button) findViewById(R.id.submit);
 
-        webView.setWebViewClient(new WebViewClient() {
-            @Override
-            public void onPageFinished(WebView view, String url) {
-                super.onPageFinished(view, url);
-                Log.d(TAG, url);
-            }
-
-            @Override
-            public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
-                super.onReceivedError(view, request, error);
-                Log.d(TAG, "Error : " + error.getErrorCode() + "");
-            }
-
-            @Override
-            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
-                handler.proceed();
-                Log.d(TAG, "SSL Error : " + error.toString());
-            }
-        });
-        webView.setWebChromeClient(new WebChromeClient());
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.getSettings().setAppCacheEnabled(true);
-        webView.getSettings().setDomStorageEnabled(true);
-        webView.loadUrl("https://10.50.0.100/connect/PortalMain");
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        final SharedPreferences.Editor editor = preferences.edit();
 
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                webView.loadUrl("javascript: {" +
-                        "document.getElementById('LoginUserPassword_auth_username').value = '" + username.getText().toString() + "';" +
-                        "document.getElementById('LoginUserPassword_auth_password').value = '" + password.getText().toString() + "';" +
-                        "var button = document.getElementById('UserCheck_Login_Button');" +
-                        "button.click(); };");
+                if(username.getText().toString().trim().equals(""))
+                    Toast.makeText(MainActivity.this, "Please enter username", Toast.LENGTH_SHORT).show();
+                else if(password.getText().toString().trim().equals(""))
+                    Toast.makeText(MainActivity.this, "Please enter username", Toast.LENGTH_SHORT).show();
+                else{
+                    editor.putString("username", username.getText().toString());
+                    editor.putString("password", password.getText().toString());
+                    editor.apply();
+                }
             }
         });
 
--- app/src/main/java/osc/dtuwifi/WiFiReceiver.java
+++ app/src/main/java/osc/dtuwifi/WiFiReceiver.java
@@ -1,26 +1,72 @@
 package osc.dtuwifi;
 
+import android.annotation.SuppressLint;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.NetworkInfo;
+import android.net.http.SslError;
 import android.net.wifi.WifiManager;
+import android.preference.PreferenceManager;
 import android.util.Log;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class WiFiReceiver extends BroadcastReceiver {
 
     public static final String TAG = "WiFiReceiver";
+    private WebView webView;
 
     public WiFiReceiver() {
     }
 
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
-    public void onReceive(Context context, Intent intent) {
+    public void onReceive(final Context context, Intent intent) {
         NetworkInfo info = intent.getParcelableExtra(WifiManager.EXTRA_NETWORK_INFO);
-        if(info != null && info.isConnected()) {
-            WifiManager wifiManager = (WifiManager)context.getSystemService(Context.WIFI_SERVICE);
-            String ssid = wifiManager.getConnectionInfo().getSSID();
-            Log.d(TAG, ssid);
+        if(info != null && info.isConnected() && info.getExtraInfo().equals("\"DTU-WiFI\"")) {
+            //FIXME : this block gets executed twice
+            webView = new WebView(context);
+            webView.setWebViewClient(new WebViewClient() {
+                @Override
+                public void onPageFinished(WebView view, String url) {
+                    super.onPageFinished(view, url);
+                    Log.d(TAG, "Page finished : " + url);
+                    if(url.equals("https://10.50.0.100/connect/PortalMain")){
+                        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+                        String username = preferences.getString("username", null);
+                        String password = preferences.getString("password", null);
+                        if(username != null && password != null){
+                            Log.d(TAG, "Logging in..");
+                            webView.loadUrl("javascript: {" +
+                                    "document.getElementById('LoginUserPassword_auth_username').value = '" + username + "';" +
+                                    "document.getElementById('LoginUserPassword_auth_password').value = '" + password + "';" +
+                                    "var button = document.getElementById('UserCheck_Login_Button');" +
+                                    "button.click(); };");
+                        }
+                    }
+                }
+
+                @Override
+                public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+                    handler.proceed();
+                    Log.d(TAG, "SSL Error : " + error.toString());
+                }
+
+                @Override
+                public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                    Log.d(TAG, "Page loaded : " + url);
+                    return super.shouldOverrideUrlLoading(view, url);
+                }
+            });
+            webView.setWebChromeClient(new WebChromeClient());
+            webView.getSettings().setJavaScriptEnabled(true);
+            webView.getSettings().setAppCacheEnabled(true);
+            webView.getSettings().setDomStorageEnabled(true);
+            webView.loadUrl("https://10.50.0.100/connect/PortalMain");
         }
     }
 }
